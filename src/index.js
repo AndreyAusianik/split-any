b1fb90bfc761dd8eeb17032db4cdc43d4e025b70
Andrei Ausianik <andrei.ausianik@iii.com>
2018-01-20 14:10:20 +0300

    split any task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function splitAny(any) {
-    // your solution here
+    if(any instanceof Array) {
+        return any.map(splitAny);
+    }
+    if(typeof any == 'string') {
+        return any.match(/\s/) ? any.split(/\s/).map(splitAny) : any.split(''); 
+    }
+    if(any instanceof Function) {
+        return splitAny(any());
+    }
+    if(any instanceof Object) {
+        return Object.keys(any).map((key) => [key, any[key]]);
+    }
+    if(typeof any == 'number') {
+        return [...any.toString()].map(c => c == '-' ? c : +c);
+    }
+    throw TypeError('wrong type passed');
 }
